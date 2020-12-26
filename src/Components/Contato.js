e0b6d57f5ca5aa34f67ef5e97b636290c6a4ed5a
Paulo Victor <pvcunha05@gmail.com>
2020-12-26 12:50:44 -0300

    Components created

---
+++ src/Components/Contato.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Contato.module.css';
+
+const Contato = () => {
+  return (
+    <div className={styles.contato}>
+      <h1>Contato</h1>
+    </div>
+  );
+};
+
+export default Contato;
